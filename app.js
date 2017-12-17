d6145d1cbe683f901ddce5b2c47c228393134d9a
RajatMauryaG <rajataitse@gmail.com>
2017-12-17 22:24:33 +0530

    Third commit

--- app.js
+++ app.js
@@ -1,5 +1,101 @@
 (function () {
   'use strict';
 
-angular.module()  
+  angular.module('ShoppingListApp', [])
+.controller('BuyController', BuyController)
+.controller('BoughtController', BoughtController)
+.provider('ShoppingListService', ShoppingListServiceProvider)
+.config(Config);
+
+//config the factory
+Config.$inject = ['ShoppingListServiceProvider'];
+function Config(ShoppingListServiceProvider) {
+  ShoppingListServiceProvider.defaults.maxItems = 10;
+}
+
+//Buy controller method
+BuyController.$inject = ['ShoppingListService'];
+function BuyController(ShoppingListService) {
+  var Buy = this;
+
+  Buy.itemsBuy = ShoppingListService.getItems();
+
+  Buy.bought = function (itemIndex) {
+    ShoppingListService.bought(itemIndex);
+  };
+
+  Buy.addtobought = function () {
+
+  };
+}
+
+
+//Bought controller method
+BoughtController.$inject = ['ShoppingListService'];
+function BoughtController(ShoppingListService) {
+  var Bought = this;
+
+  Bought.itemBought = ShoppingListService.getBougthItem();
+
+}
+
+
+//service method
+function ShoppingListService(maxItems) {
+  var service = this;
+
+  var items = [
+    {
+      name: "Milk",
+      quantity: "2"
+    },
+    {
+      name: "Donuts",
+      quantity: "200"
+    },
+    {
+      name: "Cookies",
+      quantity: "300"
+    },
+    {
+      name: "Chocolate",
+      quantity: "5"
+    },
+    {
+      name: "Peanut Butter",
+      quantity: "5"
+    }
+  ];
+
+  // service.addtobuy = function (maxitems) {
+  //
+  // };
+
+  service.getItems = function () {
+    return items;
+    };
+
+  service.bought = function (itemIndex) {
+      items.splice(itemIndex, 1);
+  };
+
+}
+
+
+//factory method
+function ShoppingListServiceProvider() {
+  var provider = this;
+
+  provider.defaults = {
+    maxItems : 10
+  };
+
+  provider.$get = function () {
+    var ShoppingList = new ShoppingListService(provider.defaults.maxItems);
+
+    return ShoppingList;
+  };
+}
+
+
 })();
